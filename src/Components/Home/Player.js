b4bc686a2d98c62fe6a6753f8e16c94513f17d1e
Faizan Arif <faizanarif777887@yahoo.com>
2022-02-17 23:08:08 +0600

    day-2 complete

--- src/Components/Home/Player.js
+++ src/Components/Home/Player.js
@@ -1,8 +1,21 @@
+//hooks
+import { useState } from 'react';
+
 //packages
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, ButtonBase } from '@mui/material';
 import ReactPlayer from 'react-player/youtube';
+import { Collapse } from 'react-collapse';
 
 const Player = () => {
+  const [details, setDetails] = useState(false);
+  /* 
+  =>false is default which I set it (hare I won't want to display the description texts so I set the state -> default false)
+  => here setDetails is set the default value, for here it is false
+  =>basically setDetails change the value of state
+  => here detail word is stored the default value, for here it is false.
+  =>
+  */
+  console.log(details);
   return (
     <Box>
       <Box>
@@ -14,6 +27,30 @@ const Player = () => {
           height={400}
         />
       </Box>
+      <Typography>
+        Post request with sending file to graphql api from next js application
+        using axios | Cabbage
+      </Typography>
+      <ButtonBase onClick={() => setDetails(!details)}>
+        {details ? 'Hide' : 'Show'} Details
+      </ButtonBase>
+      <Collapse isOpened={details}>
+        <Typography variant="body1" component="p">
+          Post request with sending file to graphql api from next js application
+          using axios | CabbagePost request with sending file to graphql api
+          from next js application using axios | CabbagePost request with
+          sending file to graphql api from next js application using axios |
+          CabbagePost request with sending file to graphql api from next js
+          application using axios | CabbagePost request with sending file to
+          graphql api from next js application using axios | CabbagePost request
+          with sending file to graphql api from next js application using axios
+          | CabbagePost request with sending file to graphql api from next js
+          application using axios | CabbagePost request with sending file to
+          graphql api from next js application using axios | CabbagePost request
+          with sending file to graphql api from next js application using axios
+          | Cabbage
+        </Typography>
+      </Collapse>
     </Box>
   );
 };
